e29b9f815573c89937d67e1dc4e2bd5deb7e7d81
BravoXavi <BravoXavi@users.noreply.github.com>
2017-12-07 12:32:23 +0100

    Font resize implemented

--- Font.cpp
+++ Font.cpp
@@ -37,12 +37,26 @@ Font::~Font()
 	//App->textures->Unload(fontPNG);
 }
 
-void Font::printText(const char* textToWrite, int x, int y) 
+void Font::printText(const char* textToWrite, int x, int y, float resize)
 {	
 	for (unsigned int i = 0; i < strlen(textToWrite); ++i) 
 	{
 		if (textToWrite[i] == ' ') simbolSize.y = 8;
 		else simbolSize = simbolPosition[traductorMap[textToWrite[i]]];
-		App->renderer->Blit(fontPNG, x + (i*8), y, &simbolSize, nullptr);
+
+		if (resize != 1.0f)
+		{
+			SDL_Rect resizeFont;
+			resizeFont.x = 0;
+			resizeFont.y = 0;
+			resizeFont.h = simbolSize.h*resize;
+			resizeFont.w = simbolSize.w*resize;
+
+			App->renderer->Blit(fontPNG, x + (i * 8), y, &simbolSize, &resizeFont);
+		}
+		else
+		{
+			App->renderer->Blit(fontPNG, x + (i * 8), y, &simbolSize, nullptr);
+		}
 	}
 }
