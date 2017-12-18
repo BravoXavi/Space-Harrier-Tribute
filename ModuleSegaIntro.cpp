af8416b3470a38bf28e087c8592b5637ea05ea0d
BravoXavi <BravoXavi@users.noreply.github.com>
2017-12-18 16:02:06 +0100

    Added new Module to the intro logo scene

---
+++ ModuleSegaIntro.cpp
@@ -0,0 +1,46 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleTextures.h"
+#include "ModuleAudio.h"
+#include "ModuleRender.h"
+#include "ModuleFadeToBlack.h"
+#include "ModuleSegaIntro.h"
+
+ModuleSegaIntro::ModuleSegaIntro(bool active) : Module(active)
+{}
+
+ModuleSegaIntro::~ModuleSegaIntro()
+{}
+
+bool ModuleSegaIntro::Start()
+{
+	segaTimer = SDL_GetTicks();
+
+	logo = App->textures->Load("assets/segalogo.png");
+
+	if (fx == 0)
+		fx = App->audio->LoadFx("assets/sfx/sega.wav");
+
+	App->audio->PlayFx(fx);
+
+	return true;
+}
+
+bool ModuleSegaIntro::CleanUp()
+{
+	App->textures->Unload(logo);
+	return true;
+}
+
+update_status ModuleSegaIntro::Update()
+{
+	App->renderer->Blit(logo, 0, 0, nullptr, nullptr);
+
+	Uint32 tick = SDL_GetTicks();
+	if (tick - segaTimer >= 4000.0f)
+	{
+		if(App->fade->isFading() == false) App->fade->FadeToBlack((Module*)App->scene_intro, this, 0.5f);
+	}
+			
+	return UPDATE_CONTINUE;
+}
